b066fe23b68420948bb0e4a50b93a5a0b42478f2
Roman Pirag <mr.anadale@yahoo.com>
2018-11-12 01:44:40 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,13 +1,13 @@
 class Hash
   def keys_of('Australia', 'Panama') 
     new_array = []
-    map do |key,value|
+    map { |key,value|
       if value
         new_array << key
       else 
         return "wat" 
       end
-    end 
+    }
   new_array.compact 
   end
 end
