7c26488ce2397ff1a7c29278e414b0bc29b97cbc
Roman Pirag <mr.anadale@yahoo.com>
2018-11-12 01:46:39 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,11 @@ class Hash
   def keys_of(*args) 
   
     map { |key,value|
-     
+      if value
+        new_array << key
+      else 
+        return nil 
+      end
     }.compact 
   end
 end
